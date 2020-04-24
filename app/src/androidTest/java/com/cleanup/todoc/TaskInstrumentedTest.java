6261b96c7b99f31b60f189b08f148be450b7afd2
Aoumama <a.oumama@gmail.com>
2020-04-24 10:07:23 +0200

    first test

--- app/src/androidTest/java/com/cleanup/todoc/TaskInstrumentedTest.java
+++ app/src/androidTest/java/com/cleanup/todoc/TaskInstrumentedTest.java
@@ -7,11 +7,20 @@ import android.support.test.runner.AndroidJUnit4;
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 
 import com.cleanup.todoc.database.TaskDatabase;
+import com.cleanup.todoc.model.Project;
+import com.cleanup.todoc.model.Task;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.Date;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class TaskInstrumentedTest {
@@ -34,4 +43,22 @@ public class TaskInstrumentedTest {
         database.close();
     }
 
+    @Test
+    public void insertTask() throws InterruptedException {
+        // add the project
+        List<Project> items = LiveDataTest.getValue(database.projectDao().getAllProjects());
+        assertEquals(0, items.size());
+
+        this.database.projectDao().insertProject(Project.getAllProjects()[0]);
+        this.database.projectDao().insertProject(Project.getAllProjects()[1]);
+        this.database.projectDao().insertProject(Project.getAllProjects()[2]);
+
+
+        items = LiveDataTest.getValue(database.projectDao().getAllProjects());
+        assertEquals(3, items.size());
+    }
+
+
 }
+
+
